7fc30e0afcedb2006b8506fd51b9358f2000a2c0
Akinlowo Olumide <adeolaolumide9@gmail.com>
2023-10-21 08:49:55 +0000

    Blog Iteration 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   resources :comments
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+  resources :posts do
+    resources :comments
+  end
